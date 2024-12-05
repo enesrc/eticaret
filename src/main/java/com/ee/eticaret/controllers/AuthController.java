e2eb3955a6d3c71848092d1b9687f4d8ad0f9fb3
enesrc <eneshsarac@gmail.com>
2024-12-05 17:37:06 +0300

    son hal

--- src/main/java/com/ee/eticaret/controllers/AuthController.java
+++ src/main/java/com/ee/eticaret/controllers/AuthController.java
@@ -3,12 +3,15 @@ package com.ee.eticaret.controllers;
 import com.ee.eticaret.entities.User;
 import com.ee.eticaret.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.concurrent.CompletableFuture;
 
 @Controller
 public class AuthController {
@@ -22,17 +25,20 @@ public class AuthController {
         return "pages/signup";
     }
 
+    @Async
     @PostMapping("/signup")
-    public String signUp(@ModelAttribute User user, RedirectAttributes redirectAttributes, Model model) {
-        boolean isRegistered = authService.signup(user);
+    public CompletableFuture<String> signUp(@ModelAttribute User user, RedirectAttributes redirectAttributes, Model model) {
+        return CompletableFuture.supplyAsync(() -> {
+            boolean isRegistered = authService.signup(user);
 
-        if(isRegistered){
-            redirectAttributes.addFlashAttribute("signupSuccess", true);
-            return "redirect:/signin";
-        } else {
-            model.addAttribute("loginError", true);
-            return "signup";
-        }
+            if (isRegistered) {
+                redirectAttributes.addFlashAttribute("signupSuccess", true);
+                return "redirect:/signin";
+            } else {
+                model.addAttribute("loginError", true);
+                return "pages/signup";
+            }
+        });
     }
 
     @GetMapping("/signin")
@@ -41,15 +47,18 @@ public class AuthController {
         return "pages/signin";
     }
 
+    @Async
     @PostMapping("/signin")
-    public String signIn(@ModelAttribute User user, Model model) {
-        boolean isAuthenticated = authService.signin(user);
-        
-        if (isAuthenticated) {
-            return "redirect:/products";
-        } else {
-            model.addAttribute("loginError", true);
-            return "signin";
-        }
+    public CompletableFuture<String> signIn(@ModelAttribute User user, Model model) {
+        return CompletableFuture.supplyAsync(() -> {
+            boolean isAuthenticated = authService.signin(user);
+
+            if (isAuthenticated) {
+                return "redirect:/products";
+            } else {
+                model.addAttribute("loginError", true);
+                return "pages/signin";
+            }
+        });
     }
 }
